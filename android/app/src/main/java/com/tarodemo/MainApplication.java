d75a63b7de69837ad681cae0ae34dccfdc6fb37c
p697 <254139147@qq.com>
2021-05-26 09:23:18 +0800

    引入lottie

--- android/app/src/main/java/com/tarodemo/MainApplication.java
+++ android/app/src/main/java/com/tarodemo/MainApplication.java
@@ -16,6 +16,8 @@ import java.util.List;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
 import org.unimodules.adapters.react.ReactModuleRegistryProvider;
 
+import com.airbnb.android.react.lottie.LottiePackage;
+
 public class MainApplication extends Application implements ReactApplication {
   private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
 
@@ -33,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           // packages.add(new RNSyanImagePickerPackage());
+          packages.add(new LottiePackage());
 
             // Add unimodules
             List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
